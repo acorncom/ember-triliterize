8856d967bb549d8840f253dec4e3b6acd3ac25a3
David Baker <dbaker@acornwebconsultants.com>
2019-09-16 16:23:05 -0500

    Setup Google Translate of the values
    Uses https://github.com/cjvnjde/google-translate-api-browser
    based on https://www.npmjs.com/package/google-translate-api

---
+++ app/components/translator/google.js
@@ -0,0 +1,19 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+import { setCORS } from "google-translate-api-browser";
+// setting up cors-anywhere server address
+const translate = setCORS("http://cors-anywhere.herokuapp.com/");
+
+export default class TranslatorGoogle extends Component {
+
+  @tracked translated = '';
+
+  @action
+  translateMe() {
+    translate(this.args.text, { from: 'ar', to: 'en' }).then(res => {
+      this.translated = res.text;
+    });
+  }
+}
